a9074c334251404aa45aea7e574b0b533ebe1e77
unknown <afveleza@eafit.edu.co>
2025-07-31 11:50:43 -0500

    Se agregaron las peliculas

--- movie/views.py
+++ movie/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Movie
+
 # Create your views here.
 
 def home(request):
     #return HttpResponse('<h1>Welcome to Home Page</h1>')
     #return render(request, 'home.html')
-    return render(request, 'home.html', {'name': 'Andres Velez'})
-eg 
+    #return render(request, 'home.html', {'name': 'Andres Velez'})
+    searchTerm = request.GET.get('searchMovie')
+    movies = Movie.objects.all()
+    
 def about(request):
-    return HttpResponse('<h1>About Us</h1>')
+    #return HttpResponse('<h1>About Us</h1>')
+    return render(request, 'about.html')
+
